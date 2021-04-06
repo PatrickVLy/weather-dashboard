917d9088cfb9b473a5f1a6a3fb1ff2f3e2e62c7c
Patrick Ly <patrickly72@gmail.com>
2021-04-05 22:33:31 -0700

    created function to pull current weather and display on the page

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ var lon;
 var forecastRow = document.getElementById("forecast");
 var searchHistory = localStorage.getItem("cities");
 var cities;
+
+function getCurrentCity(){
+    var inputCity = document.getElementById("city").value;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&units=imperial&appid="+apiKey)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+          
+        console.log(data);
+     
+    
+        console.log("city: "+ data.name);
+        cityName = data.name
+        var icon = data.weather[0].icon
+        console.log("http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
+        console.log("temperature: "+data.main.temp);
+        var temp = data.main.temp
+        console.log("humidity: "+data.main.humidity);
+        var humidity = data.main.humidity;
+        console.log("wind speed: "+data.wind.speed);
+        var windSpeed = data.wind.speed;
+        lat = data.coord.lat;
+        console.log(lat)
+        console.log("latitude: "+data.coord.lat);
+        lon = data.coord.lon;
+        console.log(lon)
+        console.log("longitude: "+data.coord.lon)
+    
+    document.getElementById("currentCity").innerHTML = cityName;
+    document.getElementById("currentTemp").innerHTML = "Temperature: "+temp+"°F ";
+    document.getElementById("icon").src= "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+    document.getElementById("currentHumidity").innerHTML = "Humidity: "+humidity+"%";
+    document.getElementById("ws").innerHTML = "Wind Speed: "+windSpeed+"mph";
+    getForecast();
+    forecastRow.setAttribute("style", "visibility: visible;");
+    cities.push(cityName);
+    localStorage.setItem("cities", JSON.stringify( cities ) );
+    addButtons();
+    })
+    
+    
+    }
